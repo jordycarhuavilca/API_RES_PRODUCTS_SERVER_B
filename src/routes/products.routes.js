998cccb94aa417a3e37748774bec3fb06c23c2a9
Jordy carhuavilca <jordycarhuavilca2021@gmail.com>
2023-10-18 18:41:17 -0500

    deleted src files because are from server_A

--- src/routes/products.routes.js
+++
@@ -1,8 +0,0 @@
-const Express = require('express')
-const router = Express.Router()
-const {addProduct,getMyPurchases} = require('../controllers/products.controller') 
-
-router.post('/checkout/:idCliente/buying',addProduct)
-router.get('/:idCliente/my_purchases',getMyPurchases)
-
-module.exports  = router
